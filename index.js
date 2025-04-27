a6037976eee0254eb764a20ef2710965f37404a8
parthrkushwaha <parthkushwahapk2003@gmail.com>
2025-04-27 23:34:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['my registration form.'],
+    typeSpeed: 90,
+    loop: false,
+    loopCount: Infinity,
+});
+function submitfunction() {
+   alert("this form is submitted");
+}
